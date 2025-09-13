facb9c3b8f1790273965e23c08ed070eeb40e1f3
Alisalam <alileramoh1@gmail.com>
2025-09-13 11:07:34 +0100

    repaired the form validation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const resultContainer = document.querySelector(".container-result");
 const form = document.querySelector("form");
 const clearBtn = document.querySelector(".clear-all");
 const submitBtn = document.querySelector(".submit-btn");
+
 /* 
 function calculatedResult() {
   resultContainer.innerHTML = "";
@@ -47,78 +48,36 @@ function calculatedResult() {
   resultContainer.classList.remove("result-home");
 } */
 
+function errorTextAdd(element) {
+  const errorText = element.parentElement.parentElement.children[1];
+  element.parentElement.style.marginBottom = "0px";
+  element.parentElement.parentElement.style.marginBottom = ".8rem";
+  errorText.innerText = "This field is required";
+  errorText.style.color = "hsl(4, 69%, 50%)";
+  errorText.style.fontSize = ".75rem";
+}
+
+function errorTextRemove(element) {
+  const errorText = element.parentElement.parentElement.children[1];
+  errorText.innerText = "";
+}
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   const principal = document.getElementById("mortgageAmount").value;
   const years = document.getElementById("mortgageTerm").value;
   const interestRate = document.getElementById("interestRate").value;
+  const radioDiv = document.querySelector(".radio-div");
+  const selectedRadio = document.querySelector(
+    'input[name="repaymentType"]:checked'
+  );
 
   const principalInvalid = principal.trim() === "";
   const yearsInvalid = years.trim() === "";
   const interestInvalid = interestRate.trim() === "";
-  if ("mortgageAmount") {
-    const element = document.getElementById("mortgageAmount");
-    element.parentElement.style.borderColor = "hsl(4, 69%, 50%)";
-    element.parentElement.firstElementChild.style.backgroundColor =
-      "hsl(4, 69%, 50%)";
-    element.parentElement.firstElementChild.style.color = "hsl(202, 86%, 94%)";
-    console.log(element.parentElement);
-  }
 
-  document.getElementById("mortgageAmount").addEventListener("input", () => {
-    const element = document.getElementById("mortgageAmount");
-    element.parentElement.style.borderColor = "";
-    element.parentElement.firstElementChild.style.backgroundColor = "";
-    element.parentElement.firstElementChild.style.color = "";
-  });
-
-  function checkError(input) {
-    if (input) {
-      const element = document.getElementById(input);
-      element.parentElement.style.borderColor = "hsl(4, 69%, 50%)";
-      element.parentElement.children[1].style.backgroundColor =
-        "hsl(4, 69%, 50%)";
-      element.parentElement.children[1].style.color = "hsl(202, 86%, 94%)";
-      console.log(element.parentElement);
-    }
-
-    document.getElementById(input).addEventListener("input", () => {
-      const element = document.getElementById(input);
-      element.parentElement.style.borderColor = "";
-      element.parentElement.children[1].style.backgroundColor = "";
-      element.parentElement.children[1].style.color = "";
-    });
-  }
-
-  checkError("mortgageTerm");
-  checkError("interestRate");
-
-  // calculation (only if inputs are valid)
-  if (!principalInvalid && !yearsInvalid && !interestInvalid) {
-    const element = document.getElementById("mortgageAmount");
-    element.parentElement.style.borderColor = "";
-    element.parentElement.firstElementChild.style.backgroundColor = "";
-    element.parentElement.firstElementChild.style.color = "";
-
-    function removeError(input) {
-      const element = document.getElementById(input);
-      element.parentElement.style.borderColor = "";
-      element.parentElement.children[1].style.backgroundColor = "";
-      element.parentElement.children[1].style.color = "";
-    }
-    removeError("mortgageTerm");
-    removeError("interestRate");
-
-    const monthlyRate = interestRate / 100 / 12;
-    const numberOfPayments = years * 12;
-    const monthlyPayment =
-      (principal * monthlyRate) /
-      (1 - Math.pow(1 + monthlyRate, -numberOfPayments));
-    const totalPayment = monthlyPayment * numberOfPayments;
-
-    /* Calculated result */
-
+  function calculatedResult(monthlyPayment, totalPayment) {
     resultContainer.innerHTML = "";
     let section1 = document.createElement("section");
     let h3Section1 = document.createElement("h3");
@@ -159,9 +118,96 @@ submitBtn.addEventListener("click", (e) => {
     resultContainer.appendChild(section1);
     resultContainer.appendChild(section2);
 
-    resultContainer.classList.remove("result-home");
+    return resultContainer.classList.remove("result-home");
+  }
 
-    console.log(totalPayment);
+  if ("mortgageAmount") {
+    const element = document.getElementById("mortgageAmount");
+    if (element.value === "") {
+      element.parentElement.style.borderColor = "red";
+      element.parentElement.firstElementChild.style.backgroundColor = "red";
+      element.parentElement.firstElementChild.style.color = "white";
+      element.parentElement.firstElementChild.style.opacity = ".9";
+      errorTextAdd(element);
+    }
+  }
+
+  document.getElementById("mortgageAmount").addEventListener("input", () => {
+    const element = document.getElementById("mortgageAmount");
+    element.parentElement.firstElementChild.style.backgroundColor = "";
+    element.parentElement.firstElementChild.style.color = "";
+    element.parentElement.style.borderColor = "";
+    errorTextRemove(element);
+  });
+
+  function checkError(input) {
+    if (input) {
+      const element = document.getElementById(input);
+      if (element.value === "") {
+        element.parentElement.style.borderColor = "red";
+        element.parentElement.children[1].style.backgroundColor = "red";
+        element.parentElement.children[1].style.color = "white";
+        element.parentElement.children[1].style.opacity = ".9";
+        errorTextAdd(element);
+      }
+    }
+    document.getElementById(input).addEventListener("input", () => {
+      const element = document.getElementById(input);
+      element.parentElement.style.borderColor = "";
+      element.parentElement.children[1].style.backgroundColor = "";
+      element.parentElement.children[1].style.color = "";
+      errorTextRemove(element);
+    });
+  }
+
+  checkError("mortgageTerm");
+  checkError("interestRate");
+
+  //Radio button confirmation
+  document.querySelectorAll('input[name="repaymentType"]').forEach((radio) => {
+    radio.addEventListener("change", () => {
+      const radioChild = radioDiv.children[2];
+      radioChild.innerText = "";
+    });
+  });
+
+  if (!document.querySelector('input[name="repaymentType"]:checked')) {
+    const radioChild = radioDiv.children[2];
+    radioChild.innerText = "This field is required";
+    radioChild.style.color = "red";
+    radioChild.style.fontSize = ".75rem";
+    console.log(radioChild);
+  } else if (selectedRadio.value === "repayment") {
+    if (!principalInvalid && !yearsInvalid && !interestInvalid) {
+      {
+        /*      const element = document.getElementById("mortgageAmount");
+      element.parentElement.style.borderColor = "";
+      element.parentElement.firstElementChild.style.backgroundColor = "";
+      element.parentElement.firstElementChild.style.color = "";
+
+      function removeError(input) {
+        const element = document.getElementById(input);
+        element.parentElement.style.borderColor = "";
+        element.parentElement.children[1].style.backgroundColor = "";
+        element.parentElement.children[1].style.color = "";
+      }
+      removeError("mortgageTerm");
+      removeError("interestRate"); */
+      }
+
+      const monthlyRate = interestRate / 100 / 12;
+      const numberOfPayments = years * 12;
+      const monthlyPayment =
+        (principal * monthlyRate) /
+        (1 - Math.pow(1 + monthlyRate, -numberOfPayments));
+      const totalPayment = monthlyPayment * numberOfPayments;
+      /* Calculated result */
+      calculatedResult(monthlyPayment, totalPayment);
+    }
+  } else {
+    let monthlyInterest = (principal * (interestRate / 100)) / 12;
+    let totalPayment = ((principal * (interestRate / 100)) / 12) * years * 12;
+    calculatedResult(monthlyInterest, totalPayment);
   }
 });
 
@@ -171,16 +217,27 @@ clearBtn.addEventListener("click", function (e) {
   document.getElementById("mortgageTerm").value = "";
   document.getElementById("interestRate").value = "";
 
+  const radioDiv = document.querySelector(".radio-div");
+  const radioChild = radioDiv.children[2];
+  radioChild.innerText = "";
+
+  const label = document.querySelector('input[name="repaymentType"]:checked');
+  if (label) {
+    label.checked = false;
+  }
+
   const element = document.getElementById("mortgageAmount");
   element.parentElement.style.borderColor = "";
   element.parentElement.firstElementChild.style.backgroundColor = "";
   element.parentElement.firstElementChild.style.color = "";
+  errorTextRemove(element);
 
   function removeError(input) {
     const element = document.getElementById(input);
     element.parentElement.style.borderColor = "";
     element.parentElement.children[1].style.backgroundColor = "";
     element.parentElement.children[1].style.color = "";
+    errorTextRemove(element);
   }
   removeError("mortgageTerm");
   removeError("interestRate");
